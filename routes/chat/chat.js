b4a8574ece00bf3b209f818ba1a1c4644d111aaf
Jepp0103 <jeppendyekjaer@hotmail.com>
2020-06-01 14:03:52 +0200

    Working on displaying username

--- routes/chat/chat.js
+++ routes/chat/chat.js
@@ -3,9 +3,9 @@ const User = require('../../models/User.js');
 const fs = require('fs');
 
 router.get('/chat', (req, res) => {
-    //const navbar = fs.readFileSync("./public/navbar/navbar.html", "utf8");
+    const navbar = fs.readFileSync("./public/navbar/navbar.html", "utf8");
     const chatPage = fs.readFileSync("./public/chat/chat.html", "utf8");
-    return res.send(/*navbar +*/ chatPage);
+    return res.send(navbar + chatPage);
 });
 
 
